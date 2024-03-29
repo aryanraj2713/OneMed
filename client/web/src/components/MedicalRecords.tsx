66b9c0ba9f7d4bca4d86992f2864763d83f61e55
tanmayagrwl <tanmayhp78@gmail.com>
2024-03-30 04:04:14 +0530

    add accordation

--- client/web/src/components/MedicalRecords.tsx
+++ client/web/src/components/MedicalRecords.tsx
@@ -1,6 +1,22 @@
-
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
 function MedicalRecords() {
-	return <div>MedicalRecords</div>;
+	return (
+		<div>
+			<Accordion type="single" collapsible>
+				<AccordionItem value="item-1">
+					<AccordionTrigger>Is it accessible?</AccordionTrigger>
+					<AccordionContent>
+						Yes. It adheres to the WAI-ARIA design pattern.
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+		</div>
+	);
 }
 
 export default MedicalRecords;
