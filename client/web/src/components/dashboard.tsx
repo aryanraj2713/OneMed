5afcf2a8b8620986eddc187747400fd81bcc20bc
tanmayagrwl <tanmayhp78@gmail.com>
2024-03-30 04:10:58 +0530

    add uniformity to the site

--- client/web/src/components/dashboard.tsx
+++ client/web/src/components/dashboard.tsx
@@ -3,7 +3,6 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import MedicalRecords from "./MedicalRecords";
 import ConsultationRecords from "./ConsultationRecords";
-import { Link } from "react-router-dom";
 
 function Dashboard() {
 	const [activeButton, setActiveButton] = useState(""); // State to track active button
@@ -16,36 +15,6 @@ function Dashboard() {
 			setActiveButton(buttonName); // Activate button
 		}
 	};
-	const [userDetails, setUserDetails] = useState({
-		id: "",
-		bloodGroup: "",
-		allergies: [],
-		insuranceCompany: "",
-		insuranceNumber: "",
-		familyDoctor: "",
-		age: "",
-		emergencyContact: "",
-		currentMedication: [],
-	});
-
-	const [searchId, setSearchId] = useState(""); // State to store search ID
-	const handleSearch = async () => {
-		try {
-			const url = "http://127.0.0.1:8000/user/" + searchId;
-			const response = await fetch(url, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			console.log(response);
-			const data = response.json();
-			console.log(data);
-
-		} catch (error) {
-			console.error(error);
-		}
-	}
 
 	return (
 		<div className="w-full h-auto min-h-screen flex flex-col gap-10 p-10">
@@ -66,15 +35,7 @@ function Dashboard() {
 				Hello Admin
 			</h1>
 			<div className="w-full h-[20%] px-10 flex items-center justify-center gap-4">
-				<Input
-					className="w-[70%] "
-					onChange={(e) => setSearchId(e.target.value)}
-					placeholder="Search by ID"
-					value={searchId}
-					type="text"
-					name="searchId"
-					id="searchId"
-				/>{" "}
+				<Input className="w-[70%] " />
 				<Button>Search</Button>
 			</div>
 			<div className="w-full flex justify-center">
@@ -157,62 +118,48 @@ function Dashboard() {
 					</div>
 				</div>
 			</div>
-			<div className="p-4 flex flex-col w-full h-auto border bg-orange-200 border-black rounded-2xl">
-				<div className="pb-5 font-bold ">
-					User's summary for medical history{" "}
-				</div>
-				<div>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-					Omnis cupiditate blanditiis quam accusamus corporis et quas
-					veniam optio delectus. Neque nisi perferendis natus odit
-					distinctio et accusantium omnis, saepe obcaecati!{" "}
+			<div className="flex w-full justify-center">
+				<div className="p-4 flex flex-col w-full h-auto border max-w-5xl bg-orange-200 border-black rounded-2xl">
+					<div className="pb-5 font-bold ">
+						User's summary for medical history{" "}
+					</div>
+					<div>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+						cupiditate blanditiis quam accusamus corporis et quas veniam optio
+						delectus. Neque nisi perferendis natus odit distinctio et
+						accusantium omnis, saepe obcaecati!{" "}
+					</div>
 				</div>
 			</div>
-			<div className="flex bg-orange-200 rounded-2xl border-black border py-10 justify-evenly">
-				<Link to="/upload-docs"><Button>
-					Upload Document
-				</Button></Link>
-				{/* <Link to="/upload-audio">
-					<Button>
-						Upload Audio
-					</Button>
-				</Link> */}
-				<Link to="/record-audio">
-					<Button>
-						Record Audio
-					</Button>
-				</Link>
-			</div>
-			<div className="w-full bg-orange-200 rounded-2xl flex flex-col">
-				<div className="flex w-full h-auto justify-center items-center ">
-					<button
-						type="button"
-						className={`w-[50%] border border-black p-5 ${activeButton === "Medical"
-							? "bg-orange-300"
-							: "bg-gray-50"
+			<div className="flex w-full justify-center">
+				<div className="w-full bg-orange-200 rounded-2xl flex flex-col max-w-5xl">
+					<div className="flex w-full h-auto justify-center items-center ">
+						<button
+							type="button"
+							className={`w-[50%] border border-black p-5 ${
+								activeButton === "Medical" ? "bg-orange-300" : "bg-gray-50"
 							} rounded-tl-xl`}
-						onClick={() => handleButtonClick("Medical")}
-					>
-						Medical Records
-					</button>
-					<button
-						type="button"
-						className={`w-[50%] border border-black p-5 ${activeButton === "Consultation"
-							? "bg-orange-300"
-							: "bg-gray-50"
+							onClick={() => handleButtonClick("Medical")}
+						>
+							Medical Records
+						</button>
+						<button
+							type="button"
+							className={`w-[50%] border border-black p-5 ${
+								activeButton === "Consultation" ? "bg-orange-300" : "bg-gray-50"
 							} rounded-tr-xl`}
-						onClick={() => handleButtonClick("Consultation")}
-					>
-						Consultation Records
-					</button>
-				</div>
-
-				<div className="p-10">
-					{activeButton === "Medical" ? (
-						<MedicalRecords />
-					) : (
-						<ConsultationRecords />
-					)}
+							onClick={() => handleButtonClick("Consultation")}
+						>
+							Consultation Records
+						</button>
+					</div>
+					<div className="p-10">
+						{activeButton === "Medical" ? (
+							<MedicalRecords />
+						) : (
+							<ConsultationRecords />
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
